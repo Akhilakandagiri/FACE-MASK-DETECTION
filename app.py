2d65ccb696819e81f5a4b1763dfb22f10ea18a86
Akhila <akhilakandagiri2005@gmail.com>
2026-07-01 22:40:31 +0530

    i did changes to the comments

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 import detect_mask_image
 
-# Setting custom Page Title and Icon with changed layout and sidebar state
+# Setting custom Page Title and Icon with changed layout and sidebar state. thank you 
 st.set_page_config(page_title='Face Mask Detector', page_icon='😷', layout='centered', initial_sidebar_state='expanded')
 
 
